1ff703ef713698d70f8dd2f276305aaaba668645
Chi-Cheong Weng <cweng@musiclifeltd.com>
2014-10-27 14:08:58 +0800

    added signout button

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.directives'])
 
-.run(function($ionicPlatform, $state) {
+.run(function($ionicPlatform, $rootScope, $state) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -18,6 +18,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    $rootScope.state='signout';
     $state.go('tab.account')
   });
 })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,22 @@
-angular.module('starter.controllers',[]).controller('AccountCtrl',['$scope','USER','$state',function($scope,USER,$state){
+angular.module('starter.controllers',[]).controller('AccountCtrl',['$rootScope','$scope','USER','$state',function($rootScope, $scope,USER,$state){
     $scope.user={};
+    if (USER.name) {
+        $scope.user.name = USER.name;
+    }
+    if (USER.phone) {
+        $scope.user.phone = USER.phone;
+    }
     $scope.signin=function(){
         USER.name=$scope.user.name;
         USER.phone=$scope.user.phone;
+        $rootScope.state='signin';
         $state.go('tab.dash');
+    }
+    $scope.signout=function(){
+        $rootScope.state='signout';
+    }
+    $scope.isSignIn=function(){
+        return $rootScope.state=='signin';
     }
 }]).controller('DashCtrl',['$scope','$rootScope','USER', '$state', 'SOCKET_URL', function($scope,$rootScope,USER,$state,SOCKET_URL){
     $scope.name = USER.name;
