57d5bd27b5b0151fd3a1f298f224b0171e34d9d0
Chi-Cheong Weng <cweng@musiclifeltd.com>
2014-11-17 11:25:52 +0800

    send user instead of user.name and user.phone in emit.request

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -52,7 +52,7 @@ angular.module('starter.controllers',[])
         $scope.address = "unknown";
     });
     $scope.request=function(){
-        data.socket.emit('rider:request', {name:data.user.name, phone:data.user.phone, address:$scope.address});
+        data.socket.emit('rider:request', {user:data.user, address:$scope.address, device:data.device});
     }
 }])
 
