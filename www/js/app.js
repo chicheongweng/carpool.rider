433a1e11ff1e8bb2a597032ca885d689d3d3c36f
Chi-Cheong Weng <cweng@musiclifeltd.com>
2014-11-21 15:55:23 +0800

    use tel inputtype for phone number input

--- www/js/app.js
+++ www/js/app.js
@@ -202,9 +202,8 @@ angular.module('starter', ['ionic', 'ngCordova', 'google-maps', 'starter.control
     });
     socket.on('requestack', function(data) {
         $rootScope.$apply(function(){
-            var message = {date:Date(), message:'Request sent to name: '+data.user.name+', phone: '+data.user.phone+'. Estimated distance: '+data.distance.text+'. Estimated duration: '+data.duration.text+'.'};
-            message.date = Date();
-            $rootScope.messages.unshift(message);
+            data.date = Date();
+            $rootScope.messages.unshift(data);
         });
     });
 
